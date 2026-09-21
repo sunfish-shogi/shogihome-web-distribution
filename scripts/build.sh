4ff26e1577272b91daeaf459a4090135feed3e28
Kubo, Ryosuke <sunfish-shogi@users.noreply.github.com>
2026-09-21 16:07:01 +0900

    Add coi headers

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,8 +5,11 @@ source ../config
 
 PROJECT_ROOT=..
 SHOGIHOME_ROOT="$PROJECT_ROOT/$SHOGIHOME_DIR_NAME"
+OUT_DIR=../dist
 
 cd "$SHOGIHOME_ROOT"
 
 echo "Build..."
-SHOGIHOME_BUILD_PROFILE=../profile.json npm run build -- --outDir ../dist
+SHOGIHOME_BUILD_PROFILE=../profile.json npm run build -- --outDir $OUT_DIR
+
+cp ../cloudflare/_headers $OUT_DIR/_headers
