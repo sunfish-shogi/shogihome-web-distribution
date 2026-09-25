fcb14ce38c24eddd42268f3633de8fa9fa9c8e17
Kubo, Ryosuke <sunfish-shogi@users.noreply.github.com>
2026-09-26 03:19:33 +0900

    Fix third party licenses

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,11 +7,13 @@ source ../config
 PROJECT_ROOT=..
 SHOGIHOME_ROOT="$PROJECT_ROOT/$SHOGIHOME_DIR_NAME"
 OUT_DIR=../dist
+DOCS_DIR=../docs
 
 cd "$SHOGIHOME_ROOT"
 
 echo "Build..."
 SHOGIHOME_BUILD_PROFILE=../profile.json npm run build -- --outDir "$OUT_DIR"
-node ./scripts/report-license.mjs "$OUT_DIR/docs"
+node ./scripts/report-license.mjs "$DOCS_DIR"
+git diff --exit-code
 
 cp ../cloudflare/_headers "$OUT_DIR/_headers"
