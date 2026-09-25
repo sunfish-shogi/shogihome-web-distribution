fc182a608fadb9b114a48fbdb3cb4417436d37c5
Kubo, Ryosuke <sunfish-shogi@users.noreply.github.com>
2026-09-26 02:30:58 +0900

    Update deploy settings

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,7 +11,7 @@ OUT_DIR=../dist
 cd "$SHOGIHOME_ROOT"
 
 echo "Build..."
-SHOGIHOME_BUILD_PROFILE=../profile.json npm run build -- --outDir $OUT_DIR
-node ./scripts/report-license.mjs $OUT_DIR/docs
+SHOGIHOME_BUILD_PROFILE=../profile.json npm run build -- --outDir "$OUT_DIR"
+node ./scripts/report-license.mjs "$OUT_DIR/docs"
 
-cp ../cloudflare/_headers $OUT_DIR/_headers
+cp ../cloudflare/_headers "$OUT_DIR/_headers"
